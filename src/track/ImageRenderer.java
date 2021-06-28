d208a32ba56f004249e15008411c0cbd5ff058a4
ckfChao <tonychao3699@gmail.com>
2021-06-29 00:14:42 +0800

    add track and ImageRenderer for render the image for track

--- src/track/ImageRenderer.java
+++ src/track/ImageRenderer.java
@@ -1,4 +1,4 @@
-package fsm;
+package track;
 
 import java.awt.*;
 
--- src/track/Track.java
+++ src/track/Track.java
@@ -4,10 +4,8 @@ import model.SpriteShape;
 import model.Sprite;
 
 import java.awt.*;
-import java.util.concurrent.CopyOnWriteArraySet;
 
 public class Track extends Sprite {
-
     private final int id;
     private final Point location;
     private final SpriteShape shape;
@@ -16,9 +14,41 @@ public class Track extends Sprite {
     public Track(int id, Point location) {
         this.location = location;
         this.id = id;
-        shape = new SpriteShape();
+        shape = new SpriteShape(new Dimension(144, 1080), 
+                                new Dimension(0, 0), new Dimension(144, 1080));
         ImageRenderer imageRenderer = new TrackImageRenderer(this);
-        //???
+    }
+
+    public int getID() {
+        return this.id;
+    }
+
+    @Override
+    public void update() {}
+
+
+    @Override
+    public void render(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(this.location.x, this.location.y, this.getBodySize().width, this.getBodySize().height);
+    }
+
+
+    @Override
+    public Dimension getBodyOffset() {
+        return shape.bodyOffset;
+    }
+
+
+    @Override
+    public Dimension getBodySize() {
+        return shape.bodySize;
+    }
+
+
+    @Override
+    public Rectangle getRange() {
+        return new Rectangle(location, shape.size);
     }
 
 

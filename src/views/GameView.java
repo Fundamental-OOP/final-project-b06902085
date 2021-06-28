4203289d1759d457f46171f9eff3c84d307f8c5a
Oscar Chew <oscarchew98@gmail.com>
2021-06-28 11:38:31 +0800

    Scale image correctly

--- src/views/GameView.java
+++ src/views/GameView.java
@@ -12,14 +12,14 @@ import java.io.*;
 import javax.imageio.ImageIO;
 
 public class GameView extends JFrame {
-    public static final int HEIGHT = 1080;
     public static final int WIDTH = 1920;
+    public static final int HEIGHT = 1080;
     private final Canvas canvas;
     private final Game game;
 
     public GameView(Game game) throws HeadlessException {
         try {
-            this.canvas = new Canvas("./assets/anm7064.jpeg");
+            this.canvas = new Canvas();
         }
         catch (IOException e)   {
             throw new RuntimeException(e);
@@ -53,24 +53,22 @@ public class GameView extends JFrame {
 
     public static class Canvas extends JPanel implements GameLoop.View{
         private Screen screen;
-        private Image backgroundImage;
-
+        private Image background;
         @Override
         public void render(Screen screen) {
             this.screen = screen;
             repaint(); // ask the JPanel to repaint, it will invoke paintComponent(g) after a while.
         }
 
-        public Canvas(String fileName) throws IOException {
-            backgroundImage = ImageIO.read(new File(fileName));
+        public Canvas() throws IOException {
+            background = ImageIO.read(new File("./assets/anm7064.jpeg"));
+            //background.getScaledInstance(WIDTH, HEIGHT, Image.SCALE_SMOOTH);
         }
 
         @Override
         protected void paintComponent(Graphics g /*paintbrush*/) {
             super.paintComponent(g);
-            g.drawImage(backgroundImage, 0, 0, this);
-            //g.fillRect(0, 0, GameView.WIDTH, GameView.HEIGHT);
-
+            g.drawImage(background, 0, 0, getWidth(), getHeight(), this);
             screen.render(g); // ask the world to paint itself and paint the sprites on the canvas
         }
     }
